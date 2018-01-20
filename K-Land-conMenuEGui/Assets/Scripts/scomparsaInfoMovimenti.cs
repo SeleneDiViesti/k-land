76d4c465be8fc081c2582b0ae76fe188c48ddf38
SeleneDiViesti <s239776@studenti.polito.it>
2018-01-20 15:59:58 +0100

    fontAlice+infoIniziali
    fontAlice+infoIniziali

---
+++ K-Land-conMenuEGui/Assets/Scripts/scomparsaInfoMovimenti.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scomparsaInfoMovimenti : MonoBehaviour {
+    public GameObject guiInfo;
+    public GameObject guiSpille;
+    // Use this for initialization
+    void Start()
+    {
+        guiInfo = GameObject.Find("InfoMovimenti");
+        guiSpille = GameObject.Find("InfoLabirintoESpille");
+        guiSpille.SetActive(false);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            guiInfo.SetActive(false);
+            guiSpille.SetActive(true);
+        }
+    }
+}
