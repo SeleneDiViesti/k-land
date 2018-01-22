a1bf4c884b5efc9b1b6e2d7cb937ab0123b3bbc9
SeleneDiViesti <s239776@studenti.polito.it>
2018-01-22 19:31:38 +0100

    animazioneBraccioCatapulta
    parte solo quando si schiaccia il tasto predefinito ma ancora non riesco a fargli fare solo una animazione

---
+++ K-Land-conMenuEGui/Assets/Scripts/attivazioneAnimazione.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attivazioneAnimazione : MonoBehaviour {
+    public GameObject GuiCatapulta;
+    public Animator anim;
+    private bool isNear = false;
+  
+    // Use this for initialization
+    void Start()
+    {
+        GuiCatapulta = GameObject.Find("GuiCatapulta");
+        GuiCatapulta.SetActive(false);
+        anim=this.GetComponent<Animator>();
+        anim.Play("New State");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && isNear==true)
+        {
+            anim.Play("catapulta_animation");
+            
+        }
+    }
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GuiCatapulta.SetActive(true);
+            isNear = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GuiCatapulta.SetActive(false);
+            isNear = false;
+        }
+    }
+}
