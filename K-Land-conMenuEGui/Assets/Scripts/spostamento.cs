23f9d88b4019bb44bed837cfbc8d5e34829f7493
Criswonderland <35038732+Criswonderland@users.noreply.github.com>
2018-01-26 19:11:31 +0100

    carte percorso , salto giù
    percorso carte trigger deselezionato (per far camminare uni sulle carte); aggiunto spostamento quando incontra lo stregatto in modo che vede la porta e poi attivare l'animazione col tasto 3

---
+++ K-Land-conMenuEGui/Assets/Scripts/spostamento.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spostamento : MonoBehaviour {
+
+
+	public GameObject spostamento_2;
+	public Vector3 mposition;
+	public Quaternion mrotation;
+	public GameObject unitychain;
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Player")) {
+			spostamento_2 = GameObject.Find ("spostamento_2");
+			mposition = spostamento_2.transform.position;
+			mrotation = spostamento_2.transform.rotation;
+			unitychain = GameObject.FindGameObjectWithTag ("Player");
+
+			unitychain.transform.SetPositionAndRotation (mposition, mrotation);
+
+
+		}
+	}
+
+
+}
