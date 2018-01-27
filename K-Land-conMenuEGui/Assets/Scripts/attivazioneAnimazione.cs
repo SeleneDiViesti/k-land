0f6bc2381b000c0038ac47a46fa977e352b69035
SeleneDiViesti <s239776@studenti.polito.it>
2018-01-27 10:54:31 +0100

    animazione catapulta corretta

--- K-Land-conMenuEGui/Assets/Scripts/attivazioneAnimazione.cs
+++ K-Land-conMenuEGui/Assets/Scripts/attivazioneAnimazione.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(Animator))]
 
 public class attivazioneAnimazione : MonoBehaviour {
     public GameObject GuiCatapulta;
     public GameObject braccio;
-    public Animator anim;
     private bool isNear = false;
-  
+    public Animator anim;
+    public AnimatorStateInfo currentBaseState;
+
+    static int idleState = Animator.StringToHash("Base Layer.New State");
+    static int animazione = Animator.StringToHash("Base Layer.catapulta_animation");
+   
+
     // Use this for initialization
     void Start()
     {
@@ -15,19 +21,32 @@ public class attivazioneAnimazione : MonoBehaviour {
         GuiCatapulta = GameObject.Find("GuiCatapulta");
         GuiCatapulta.SetActive(false);
         anim= braccio.GetComponent<Animator>();
-        anim.Play("New State");
+        
+        //anim.Play("New State");
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Z) && isNear==true)
+        currentBaseState = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (Input.GetKeyDown(KeyCode.Z) && isNear == true)
         {
-            anim.Play("catapulta_animation");
-            
+            if (currentBaseState.nameHash == idleState)
+            {
+            anim.SetBool("zKey", true);
+            }
+        }
+
+        //anim.Play("catapulta_animation");
+        if (currentBaseState.nameHash == animazione)
+        {
+            if (!anim.IsInTransition(0))
+            {
+                anim.SetBool("zKey", false);
+            }
         }
     }
-    
-
+        
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -35,7 +54,6 @@ public class attivazioneAnimazione : MonoBehaviour {
             GuiCatapulta.SetActive(true);
             isNear = true;
         }
-
     }
 
     void OnTriggerExit(Collider other)
