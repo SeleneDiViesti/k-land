7f6b137980fdd378f6677e98ca8dba5635753385
Criswonderland <35038732+Criswonderland@users.noreply.github.com>
2018-02-05 18:50:11 +0100

    coniglio livello 1
    animazione di un coniglio che corre nel labirinto ( problema che non segue l'empty ma rimane sul posto e cammina sullo stesso punto)

---
+++ K-Land-conMenuEGui/Assets/Scripts/percorso_rabbit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class percorso_rabbit : MonoBehaviour {
+
+	public GameObject rabbit_percorso;
+	public Vector3 pos;
+
+	//public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+	//	anim = GetComponent<Animator> ();	
+
+		rabbit_percorso = GameObject.Find ("rabbit_percorso");
+
+		pos = rabbit_percorso.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	anim.Play ("percorso_empty_rabbit"); 
+
+		transform.position = pos;
+
+	}
+}
