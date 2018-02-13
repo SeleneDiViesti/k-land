846c817f6fa77415433947048d7204ae43d0e037
Criswonderland <35038732+Criswonderland@users.noreply.github.com>
2018-02-13 13:00:29 +0100

    aggiunti altri audio
    fino al livello 3

---
+++ K-Land-conMenuEGui/Assets/Scripts/audio_controller_1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class audio_controller_1 : MonoBehaviour {
+
+	public AudioSource song1;
+	public AudioSource song2;
+
+	// Use this for initialization
+	void Awake () {
+		song1.GetComponent<AudioSource> ();
+		song2.GetComponent<AudioSource> ();
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			song1.Stop ();
+			song2.Play ();
+
+		}
+	}
+}
