9bf3f3506f21f18007a2f1ed4cdaf853a77bd46e
Criswonderland <35038732+Criswonderland@users.noreply.github.com>
2018-02-13 20:07:42 +0100

    livello 4
    tutte le sfere hanno un video, in base ai collider degli elementi( cubi) avviare l'animazione; problema: creare singole animazioni altrimenti non funziona

---
+++ K-Land-conMenuEGui/Assets/Scripts/attiva_video_sfera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class attiva_video_sfera : MonoBehaviour {
+
+	public VideoPlayer video;
+	public Animator anim;
+
+	void Awake(){
+		video.GetComponent<VideoPlayer> ();
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			if (Input.GetKeyDown ("p")) {
+				// animazione sfera 
+				anim.Play("sfera_animation");
+				video.Play ();
+			}
+
+		}
+	}
+}
