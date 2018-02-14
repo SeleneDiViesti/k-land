3ed284ed9c7354c3bebc6e0e874488db732de21e
SeleneDiViesti <s239776@studenti.polito.it>
2018-02-14 18:10:27 +0100

    cancellazion video "intro" senza musica

---
+++ K-Land-conMenuEGui/Assets/Scripts/stopSong8.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stopSong8 : MonoBehaviour {
+
+    public AudioSource song8;
+	// Use this for initialization
+	void Start () {
+        song8.GetComponent<AudioSource>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            song8.Stop();
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
