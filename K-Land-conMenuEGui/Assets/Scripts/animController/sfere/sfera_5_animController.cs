c48fdc1e4efedef5986ad389b403df58184938a2
Criswonderland <35038732+Criswonderland@users.noreply.github.com>
2018-02-14 17:23:03 +0100

    sfere script modificati

--- K-Land-conMenuEGui/Assets/Scripts/animController/sfere/sfera_5_animController.cs
+++ K-Land-conMenuEGui/Assets/Scripts/animController/sfere/sfera_5_animController.cs
@@ -5,76 +5,72 @@ using UnityEngine.Video;
 
 public class sfera_5_animController : MonoBehaviour {
 
-	public Animator Sphere_1;
-	public Animator Sphere_2;
-	public Animator Sphere_3;
-	public Animator Sphere_4;
 	public Animator Sphere_5;
-	public Animator Sphere_6;
-	public Animator Sphere_7;
-	public Animator Sphere_8;
 
-	public VideoPlayer video_s_1;
-	public VideoPlayer video_s_2;
-	public VideoPlayer video_s_3;
-	public VideoPlayer video_s_4;
 	public VideoPlayer video_s_5;
-	public VideoPlayer video_s_6;
-	public VideoPlayer video_s_7;
-	public VideoPlayer video_s_8;
 
 	void Awake(){
-		video_s_1.GetComponent<VideoPlayer> ();
-		video_s_2.GetComponent<VideoPlayer> ();
-		video_s_3.GetComponent<VideoPlayer> ();
-		video_s_4.GetComponent<VideoPlayer> ();
+
 		video_s_5.GetComponent<VideoPlayer> ();
-		video_s_6.GetComponent<VideoPlayer> ();
-		video_s_7.GetComponent<VideoPlayer> ();
-		video_s_8.GetComponent<VideoPlayer> ();
+	
 	}
 
 	// Use this for initialization
 	void Start () {
-		Sphere_1 = GetComponent<Animator> ();
-		Sphere_2 = GetComponent<Animator> ();
-		Sphere_3 = GetComponent<Animator> ();
-		Sphere_4 = GetComponent<Animator> ();
+
 		Sphere_5 = GetComponent<Animator> ();
-		Sphere_6 = GetComponent<Animator> ();
-		Sphere_7 = GetComponent<Animator> ();
-		Sphere_8 = GetComponent<Animator> ();
+	
 	}
 
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetKeyDown ("x")) {			
+			video_s_5.Stop ();
+			Sphere_5.enabled = false;
+
+		}
+
+		if (Input.GetKeyDown ("c")) {			
+			video_s_5.Stop ();
+			Sphere_5.enabled = false;
+
+		}
+
+		if (Input.GetKeyDown ("v")) {
+			video_s_5.Stop ();
+			Sphere_5.enabled = false;
+
+		}
+
+		if (Input.GetKeyDown ("b")) {			
+			video_s_5.Stop ();
+			Sphere_5.enabled = false;
+
+		}
+
 		if (Input.GetKeyDown ("d")) {
 			
-			Sphere_1.Play ("fermo1");
-			video_s_1.Stop ();
-
-			Sphere_2.Play ("fermo2");
-			video_s_2.Stop ();
-
-			Sphere_3.Play ("fermo3");
-			video_s_3.Stop ();
-
-			Sphere_4.Play ("fermo4");
-			video_s_4.Stop ();
-
-			Sphere_6.Play ("fermo6");
-			video_s_6.Stop ();
-
-			Sphere_7.Play ("fermo7");
-			video_s_7.Stop ();
-
-			Sphere_8.Play ("fermo8");
-			video_s_8.Stop ();
-
-			//Sphere_5.enabled = true;
-
 			video_s_5.Play ();
 			Sphere_5.Play("sfera_5_animation");
+
+		}
+
+		if (Input.GetKeyDown ("f")) {			
+			video_s_5.Stop ();
+			Sphere_5.enabled = false;
+
+		}
+
+		if (Input.GetKeyDown ("g")) {			
+			video_s_5.Stop ();
+			Sphere_5.enabled = false;
+
+		}
+
+		if (Input.GetKeyDown ("h")) {			
+			video_s_5.Stop ();
+			Sphere_5.enabled = false;
 
 		}
 	}
